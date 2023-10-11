2452e0181cb47b9c82e2766db25fab266e8b7fcd
SoundharyaVivekanandan <114381070+SoundharyaVivekanandan@users.noreply.github.com>
2023-10-11 17:09:46 +0530

    Update main.tf

--- load_balancer/main.tf
+++ load_balancer/main.tf
@@ -60,16 +60,3 @@ resource "aws_lb_listener" "example_listener" {
     }
   }
 }
-/* resource "aws_lb_target_group" "jenkins_target_group" {
-  name     = "jenkins-target-group"
-  port     = 8080 
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
-}
-
- 
-
-resource "aws_lb_target_group_attachment" "jenkins_target" {
-  target_group_arn = aws_lb_target_group.jenkins_target_group.arn
-   target_id        = "Built-In Node" 
-} */
