ce048e823f3843e06dc1881589630b75acbc8abb
SoundharyaVivekanandan <114381070+SoundharyaVivekanandan@users.noreply.github.com>
2023-10-11 17:10:49 +0530

    Delete acm_certificate/variable.tf

--- acm_certificate/variable.tf
+++
@@ -1,9 +0,0 @@
-/* variable "domain_name" {
-  description = "The domain name for the SSL certificate."
-  type        = string
-}
-
-variable "route53_zone_id" {
-  description = "The ID of the Route 53 hosted zone for DNS validation."
-  type        = string
-} */
